2f616dc4394f82782a274bb0291356331b6e9086
kensakukomatsu <kensaku.komatsu@gmail.com>
2013-08-26 19:46:18 +0900

    add favicon

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(__dirname+'/public/images/favicon2013.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
