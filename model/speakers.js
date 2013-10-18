8bbf7eccfb58e0f81ed4fea47783edacbe733136
kensakukomatsu <kensaku.komatsu@gmail.com>
2013-10-18 23:15:27 +0900

    for issue 31 generate speaker page from json

---
+++ model/speakers.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "kensaku_komatsu": {
+    "name": "小松 健作",
+    "url": "http://example.net",
+    "affiliation": "NTTコミュニケーションズ",
+    "img_url": "img/_dummy_100x100.png",
+    "description": "<p>せつめい</p><p>せつめい</p><p>せつめい</p>"
+  },
+  "masataka_yakura": {
+    "name": "矢倉 眞隆",
+    "url": "http://example.net",
+    "affiliation": "html5j",
+    "img_url": "img/_dummy_100x100.png",
+    "description": "<p>せつめい</p><p>せつめい</p><p>せつめい</p>"
+  },
+  "shumpei_shiraishi": {
+    "name": "白石 俊平",
+    "url": "http://example.net",
+    "affiliation": "JOWA",
+    "img_url": "img/_dummy_100x100.png",
+    "description": "<p>せつめい</p><p>せつめい</p><p>せつめい</p>"
+  },
+  "dummy0": {
+    "name": "ダミー0",
+    "url": "http://example.net",
+    "affiliation": "dummy company",
+    "img_url": "img/_dummy_100x100.png",
+    "description": "<p>せつめい</p><p>せつめい</p><p>せつめい</p>"
+  }
+}
--- routes/conf2013.js
+++ routes/conf2013.js
@@ -1,10 +1,12 @@
 var sponsors = require('../model/sponsors')
+	, speakers = require('../model/speakers')
 
 // Helper functions
 function show_logos(arr) {
 	var _header = '<ul class="list-unstyled list-inline">'
-		, _footer = '</ul>'
 		, _body = '<li><a class="thumbnail" href="{{href}}"><img src="{{img_url}}" alt="{{alt}}"></a>'
+		, _footer = '</ul>'
+
 	var ret = [];
 
 	ret.push(_header);
@@ -20,6 +22,33 @@ function show_logos(arr) {
 	ret.push(_footer)
 
 	return ret.join("\n")
+}
+
+function show_profiles(obj){
+	var _body = [
+		'<section class="speaker">', 
+	    '<h2 id="{{speaker_id}}"><a href="{{speaker_url}}">{{name}}</a></h2>',
+	    '<p class="affiliation">{{affiliation}}</p>',
+	    '<p class="image"><img src="{{img_url}}" alt="{{img_alt}}"></p>',
+	    '<div class="description">{{description}}</div>',
+	  	'</section>'
+  	].join("\n");
+
+  	var ret = []
+
+  	for(var key in obj) if(obj.hasOwnProperty(key)){
+  		ret.push(_body
+  			.replace("{{speaker_id}}", key)
+  			.replace("{{speaker_url}}", obj[key].url)
+  			.replace("{{name}}", obj[key].name)
+  			.replace("{{affiliation}}", obj[key].affiliation)
+  			.replace("{{img_url}}", obj[key].img_url)
+  			.replace("{{img_alt}}", obj[key].name)
+  			.replace("{{description}}", obj[key].description)
+  		)
+  	}
+
+  	return ret.join("\n")
 }
 
 /*
@@ -46,6 +75,8 @@ exports.speakers = function (req, res) {
   res.render("2013/speakers", {
   	id: "speakers"
   	, title: "スピーカー | HTML5 Conference 2013" 
+  	, speakers: speakers
+  	, show_profiles: show_profiles
   });
 };
 
