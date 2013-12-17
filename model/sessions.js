49c11c99bbb19d6ae8b06c102c65a156d9c56512
Masataka Yakura <masataka.yakura@gmail.com>
2013-12-18 00:52:58 +0900

    セッションの映像をmodelに追加。

--- model/sessions.js
+++ model/sessions.js
@@ -8,42 +8,48 @@ module.exports = [
         "title": "現場の実例から学ぶ、最新鋭のWebアプリケーション開発フローとアーキテクチャ",
         "speakers": ["go_otani", "kei_funagayama"],
         "description": "フロントエンド技術の急速な発展により、フロントエンド開発者に求められる技術・知識もどんどん変わりつつあります。$$$このセッションでは、サイバーエージェントにおける実際の開発事例をもとに、最新鋭のWebアプリケーションの開発フローや、実際のアーキテクチャを構成している先端技術の組み合わせ方などをご紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=GNpm5vpalbE",
+        "slides": null
       },
       {
         "id" : "2f-l-1",
         "title": "ようこそ、HTML5裏APIの世界へ",
         "speakers": ["futomi_hatano"],
         "description": "Canvas、WebGL、WebRTC、WebSocketなど、HTML5の花形スターとも言えるモテ系APIは、常に世間の注目を集めている。これらAPIを使いこなせるウェブディベロッパーはどこからも引っ張りだこだろう。しかし、注目度が低いながらも、今後のウェブを支える（かもしれない）最新のAPIが数多く存在する。このようなAPIは派手さが足りないゆえに話題になることもない。しかし、これら非モテ系のAPIも含めてHTML5だ。$$$本セッションでは、ありきたりのモテ系APIに飽きたマニアな貴方のために、普段は陽の当たらないAPIを一挙紹介する。もちろん、どれかのブラウザーに実装されているAPIのみだ。今から使おうと思えば使えないことはない。そして、W3Cにて仕様策定が始まって日が浅いため、明日にはどうなるか分からない。無くなるかもしれないし、大幅に変更されてしまうかもしれない。今覚えても役に立たないかもしれないし、役に立つかもしれない。そんなスリリングなAPIを知ってこそマニアなのだ。$$$ようこそ、HTML5裏APIの世界へ。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=98MKy-0K8bU",
+        "slides": null
       },
       {
         "id" : "5f-l-1",
         "title": "モバイル時代のフロントエンド開発レシピ",
         "speakers": ["yuya_saito"],
         "description": "改めて言うまでもなく、モバイルWebの時代はもうすぐそこに来ています。現時点で日本はまだまだ30%以下というスマートフォン普及率ではありますが、今後確実に成長していく分野であることは間違いありません。$$$今回のセッションではこれから本格的にモバイルWebの開発を始める皆さんに、押さえておきたいトレンドについて、そして、先駆者達が重ねてきた失敗から得た成功へのヒントを紹介していきます。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=SjBMIPf1e5I",
+        "slides": null
       },
       {
         "id" : "5f-s0-1",
         "title": "真面目なアニメーション",
         "speakers": ["brian_birtles"],
         "description": "直感的なUIを作ったり、物事を分かりやすく伝えるにはアニメーションが非常に有効ですがWebではアニメーション技術はまだまだ未熟です。従来のCSSやSVGあるいはJavascriptのアニメーションと比べ、Web Animationsはより高速かつ高機能なアニメーションを実現できます。このセッションではWeb Animationsの機能や使い方をデモしながら紹介していきます。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=ZaAsy8CyTqc",
+        "slides": null
       },
       {
         "id" : "5f-s1-1",
         "title": "Markup Maniax - マークアップ部が語り合うHTML5仕様のいま・これから",
         "speakers": ["masakazu_muraoka", "yu_sakai", "masataka_yakura"],
         "description": "HTML5といえばAPIや周辺技術の話題を目にすることがほとんどになりましたが、Webサイトで重要なのはやはりマークアップ。HTML5がLast Callになってからも仕様に変更が加わってます。また、独自の要素を定義できる仕組みも登場しました。マークアップの今後はどう向かうのか、マークアップ部の3人が語ります。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=tfGqbWoAKUs",
+        "slides": null
       },
       {
         "id" : "6f-l-1",
         "title": "WebSocket, WebRTC, Socket API, ... 最新Webプロトコルの傾向と対策",
         "speakers": ["kensaku_komatsu"],
         "description": "WebはNWと深いかかわりがあります。従来より、CSSスプライトや concurrent HTTP など HTTP の制約に根ざした様々な最適化プラクティスが Web professional の間では用いられてきました。そして今 WebSocket, WebRTC, Socket API など新たな通信系 API が現れ、Webアプリケーションの可能性が大きく拡がろうとしている一方、head of line blocking や UDP hole punching など、TCP/IPレベルのより深い知識が Web professional には要求されるようになってきています。$$$本セッションでは、このような「これからのWeb」と「ネットワーク」との関わりや抑えておくべきポイントについて、その「傾向と対策」を紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=nXhEDC2qOLg",
+        "slides": null
       }
     ]
   },
@@ -61,42 +67,48 @@ module.exports = [
         "title": "Responsive Web Design 〜Basic understanding〜",
         "speakers": ["daisuke_yamazaki"],
         "description": "マルチデバイス対応（各デバイスに合わせたサイト制作）の必要性がでてきている昨今、レスポンシブWebデザイン（略してRWD）のキーワードを聞くようになりました。今回はRWDの概念〜基本的な考え方〜制作方法について解説させていただきます。RWDは制作手法の考え方、手法の１つですので正しく理解していただければと思います。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=biqU9VK4Y1M",
+        "slides": null
       },
       {
         "id" : "2f-l-2",
         "title": "HTML5とIE11とWindows 8.1",
         "speakers": ["yoshitaka_kasugai"],
         "description": "2011年に米国・ラスベガスで開催されたMIX 11というカンファレンスでマイクロソフトは「HTML5 : Native to Windows」という言葉を使いました。$$$それから2年後、昨年10月にリリースされたWindows 8では、この言葉の通りWindows 8のネイティブアプリをHTML5とCSS3、JavaScriptで開発できるフレームワークを実装しました。一方でIE9、IE10、そして、IE11とマイクロソフトのブラウザは着実にHTML5への対応を進めており、IE11ではWebGL、MPEG-DASH、Encrypted Media Extensions (EME)などに対応しました。$$$このセッションではIE11で新たにサポートされるHTML5、Windows 8.1のアプリとHTML5との関係、そして、現在のマイクロソフトのHTML5との向き合い方について、ご紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=12x-xi0ln20",
+        "slides": null
       },
       {
         "id" : "5f-l-2",
         "title": "実践的なモバイルHTML5テクニック",
         "speakers": ["takuo_kihira"],
         "description": "モバイルブラウザで動くHTML5は、PCブラウザのHTML5と比べて特性が大きく変わります。何も考えずに作成すると次々に課題が発生し、破綻してしまうことも少なくありません。$$$このセッションでは、私の今までの経験を踏まえ、モバイルHTML5の開発に役立つ知識やテクニックをご紹介します。Canvasをメインにお話をしますが、バッテリーやメモリ管理などの普遍的な内容も触れていきたいと考えております。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=6HTzCI3wd-0",
+        "slides": null
       },
       {
         "id" : "5f-s0-2",
         "title": "オフラインWebアプリケーションの「選択肢」",
         "speakers": ["kyo_ago"],
         "description": "最近注目される「オフラインWebアプリケーション」$$$このセッションでは実現にあたって考えなくてはいけない「選択肢」を取り上げ、それぞれに対してどう対処していくかを紹介したいと思います。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=RtyLYpbG3B0",
+        "slides": null
       },
       {
         "id" : "5f-s1-2",
         "title": "モバイルフロンティア：フロントエンドエンジニアとデザイナーのためのモバイルユーザーエクスペリエンス",
         "speakers": ["yukio_andoh", "nobuya_sato", "kaoru_shimizu", "hiroko_nozawa", "yoshiki_hayama", "yoshinori_wakizaka"],
         "description": "「モバイルフロンティア よりよいモバイルUXを生み出すためのデザインガイド」を翻訳したメンバーによる、ユーザーエクスペリエンス講座。http://www.amazon.co.jp/dp/4621086553$$$ただ「つくる」だけでなく良い体験をデザインすることが重要な開発フローと認識され始めている近年、ネイティブアプリ・Webアプリ関係なく「体験のデザイン」を一緒に考えていく40分。デザイナー・エンジニア含むすべての人を対象。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=R67YRT9FZoM",
+        "slides": null
       },
       {
         "id" : "6f-l-2",
         "title": "次世代テレビを知る！ハイブリッドキャスト×HTML5",
         "speakers": ["satoshi_shoda"],
         "description": "ハイブリッドキャストは、拡張したユーザが拡張したテレビを享受するための基盤技術となり得ると共に、技術サイドから次世代のテレビに大きな概念チェンジをもたらすものです。$$$これからハイブリッドキャストの開かれたAPIによって、多くの魅力的なアプリケーションが登場し、テレビの楽しみ方を拡大するでしょう。$$$本セッションでは、ハイブリッドキャストの概要、アプリケーション開発における検討事項、簡単な実装テクニックなどを紹介する予定です。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=PuLmtHSkCPg",
+        "slides": null
       }
     ]
   },
@@ -114,42 +126,48 @@ module.exports = [
         "title": "CSS Regionsを使った新しいCSSレイアウトを作る方法",
         "speakers": ["keisuke_todoroki"],
         "description": "Webコンテンツをリッチに魅せるCSSの新しい仕様が続々と準備されています。$$$FlexboxやMulti-Columnなど、これまで実現が困難だったレイアウトを簡単にしてくれる仕様が幾つかあります。このセッションでは、iOS7 のSafariでもサポートされたCSS Regionsにフォーカスして、実際のコードを確認しながら、これからの新しいWebコンテンツの作り方とその可能性を見ていきます。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=U7qOCWhWFZ0",
+        "slides": null
       },
       {
         "id" : "2f-l-3",
         "title": "Canvas/WebGLを活用したインタラクティブ表現 〜CreateJS、Three.js、Away3D.js〜",
         "speakers": ["yasunobu_ikeda"],
         "description": "Web標準で実現できるインタラクティブ表現は日々進化しています。今やスマートフォンやタブレッドでも60fpsのアニメーションを再生することが可能であり、ネイティブアプリに劣らない表現がブラウザ上で実現することができるようになっています。$$$それを実現する主な技術としてcanvasタグが知られていますが、そこに用意されているAPIは低レベルであるためクリエイターにとって決して扱いやすいものとは言えません。コンテンツの生産性を高めるにはフレームワークを利用することが一般的になっています。$$$このセッションでは、現在注目されている2DのフレームワークとしてCreateJSを、3DのフレームワークとしてThree.jsとAway3D.jsを取り上げ、Webでのインタラクティブ表現の可能性と未来を紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=Nl_ckcAOosE",
+        "slides": null
       },
       {
         "id" : "5f-l-3",
         "title": "満足させるハイブリッドアプリを作るための実践テクニック",
         "speakers": ["masahiro_tanaka"],
         "description": "『アプリがHTML5かネイティブかはユーザーには関係ない。重要なのは、そのアプリが提供するユーザーエクスペリエンスである。』$$$PhoneGapのようにHTML5でモバイルアプリを開発する手法は、簡単にアプリ制作ができる一方で、性能や品質面に課題を抱えるケースが少なくありません。このセッションでは、これまでMonacaの提供を通じて学んできた様々な落とし穴とその解決策を紹介しながら、ユーザーに満足して使ってもらえるモバイルアプリの開発テクニックを説明します。$$$本セッションの主な対象者は、HTML5を用いてモバイルアプリ開発をしている、もしくは、開発検討をしている方になりますが、ハイブリッドアプリ開発が未経験のWebデザイナーの方にも使えるテクニックも盛り込みます。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=X-vGxJUZm6g",
+        "slides": null
       },
       {
         "id" : "5f-s0-3",
         "title": "マルチデバイス時代のHTML5 & WAI-ARIA",
         "speakers": ["takeshi_kurosawa"],
         "description": "スマートデバイスやテレビを始めとして、Webを利用する環境は広がり続けています。このような状況では特定の環境によらずWebサイトが使えること=アクセシビリティの重要性が増しています。一方で、HTML5やWAI-ARIAを使ってアクセシビリティを高める方法や注意点が語られる機会は多くありませんでした。本セッションではHTML5やWAI-ARIAの活用方法や注意点を、デモを交えながら紹介いたします。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=4R1HIfBbflo",
+        "slides": null
       },
       {
         "id" : "5f-s1-3",
         "title": "Spec EditorとContributorが語るWeb標準化と開発者への期待",
         "speakers": ["takuya_oikawa", "koji_ishii", "kinuko_yasuda", "kensaku_komatsu", "masataka_yakura"],
         "description": "",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=qIL4QyxlrrA",
+        "slides": null
       },
       {
         "id" : "6f-l-3",
         "title": "HTTP/2.0がもたらすWebサービスの進化",
         "speakers": ["shigeki_ohtsu", "tatsuya_hayashi", "hirotaka_nakajima"],
         "description": "GoogleやTwitterが大規模に導入しているSPDYをベースとして、HTTP/2.0の標準化作業が現在急ピッチで進められています。$$$十数年ぶりに改訂されるこの新プロトコルによってWebサービスが今後どう変わるのか、デモを交えてHTTP/2.0の概要について解説します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=_6KwzTEqiWw",
+        "slides": null
       }
     ]
   },
@@ -167,42 +185,48 @@ module.exports = [
         "title": "モダンなCSS設計パターンを考える",
         "speakers": ["hiroki_tani"],
         "description": "昨今のフロントエンド開発では、JavaScriptを使ったリッチな実装およびそのアーキテクチャにフォーカスされがちですが、HTML/CSSもまたきちんと設計されていなければ成立しません。$$$本セッションでは、CSS設計にフォーカスし、開発・改修効率をあげるための手法などを紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=BlSFXdnuq5E",
+        "slides": null
       },
       {
         "id" : "2f-l-4",
         "title": "HTML5 run anywhere",
         "speakers": ["kosuke_nagano"],
         "description": "HTML5の世界はPCやスマホ向け以外にも広がってきています。例えば、テレビやカーナビ、ゲーム機、デジタルサイネージ、家電のUIなどなど…$$$Web技術者の仕事は、もはやWebブラウザの中だけにあるのではありません。あなたの持っているスキルをそのまま活かして、これまでとは全く異なる様々な業種で活躍できる時代が、もうすぐそこに来ています。$$$本セッションでは、Webブラウザ以外のプラットフォームが備える様々なJavaScript APIに触れながら、HTML5/JavaScriptが持つ非常に大きな可能性について紹介します。「組込み＋HTML5」というあまり話されてこなかったジャンルについて、興味を持っていただけたら幸いです。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=WDmImKw0vf0",
+        "slides": null
       },
       {
         "id" : "5f-l-4",
         "title": "地下鉄 x サクサク x これからのWebゲームアプリが備えるべき8つの機能",
         "speakers": ["takao_obara"],
         "description": "電波の不確かな場所でも動作し、サクサクと軽快に動作する Web Application の実装方法とそのアイデアについてご紹介します。$$$このセッションでは、更に一歩進んだ発想(そして、ちょっとした黒魔術)もご紹介する予定です。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=KxejnlIa_9k",
+        "slides": null
       },
       {
         "id" : "5f-s0-4",
         "title": "ブラウザとMIDIの出会いが拓くWebとMusicの新しい世界",
         "speakers": ["ryoya_kawai"],
         "description": "Web MIDI APIはWeb Audio APIと並ぶ音楽系のAPIです。あまり馴染みのないMIDIですが、Web MIDI APIを使うことであらゆる電子楽器とブラウザの接続が簡単にでき電子楽器からブラウザ上のアプリケーションを操作、また逆に、ブラウザ上のアプリケーションから電子楽器を操作することが可能です。$$$本セッションではMIDIの基礎、Web MIDI APIの概要、また10月末に発表された音声合成技術をハードウェアに搭載したLSI「NSX-1」を1ボードに収めたeVY1 SHIELDを使ったデモアプリケーション等のご紹介をまじえWebとMusicの世界についてお話させていただきます。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=ACM1BAvJYWQ",
+        "slides": null
       },
       {
         "id" : "5f-s1-4",
         "title": "(白石俊平と) カッコいいやつら 番外編 「スタートアップのススメ」",
         "speakers": ["shumpei_shiraishi", "masahiko_adachi", "nao_tokui", "daisuke_yamashita"],
         "description": "昨今の技術者にとっては、どうにも気になるキーワード、「スタートアップ」。$$$アプリ開発がどんどん容易になり、マネタイズの障壁も下がっていることで、開発者であれば誰でもスタートアップを興せる可能性がある時代です。$$$スタートアップは、きっと楽しい。でも、踏み出せない要因もたくさん。$$$潰れちゃったらどうなるんだろう・・・？家族もいるし、あんまり若くないし・・・。経営とかってよくわからないし・・・。キツイ？薄給？などなど。$$$このセッションでは、技術者としても起業家としても著名なお三方をお迎えして、技術系スタートアップの実際をとことん掘り下げます。$$$日本に、スタートアップのビッグウェーブを起こそう！",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=NPJvrskwuaU",
+        "slides": null
       },
       {
         "id" : "6f-l-4",
         "title": "進化を続ける JavaScript 〜次世代言語のステキな機能と高速化の行方〜",
         "speakers": ["tomoya_asai"],
         "description": "Web 技術が大規模アプリや OS の基盤技術となる中、そのコアである JavaScript 言語も大きな進化を遂げつつあります。$$$ECMAScript 6th などとして標準化が進められている次世代 JavaScript 言語仕様のうち、大規模開発に便利な機能や開発を効率化するステキな機能について、ブラウザで実装が進んでいるものを中心に解説します。$$$JavaScript は速度面でも Java を追い越し C 言語に迫ろうとしていますが、そのために言語とエンジンの改良がどのように進められているかご紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=22MgGmhcmPs",
+        "slides": null
       }
     ]
   },
@@ -220,42 +244,48 @@ module.exports = [
         "title": "今どきのGruntを使ったフロントエンド開発（HTML/CSS編）",
         "speakers": ["koji_ishimoto"],
         "description": "SassなどのCSSプリロセッサを使うWebデザイナが増えてきました。Sassをコンパイルするだけなら黒い画面（ターミナル）を使わずともGUIアプリからの利用で問題ありません。が、ここは一歩踏み込んでGrunt（JavaScript製のタスクランナー）を使って、Sass以外のコンパイルやライブリロード、画像最適化、CSSのリントやスタイルガイド生成など、あらゆる作業をGruntに任せてより効率的な開発環境を手に入れてみませんか。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=b4G81PkZUPc",
+        "slides": null
       },
       {
         "id" : "2f-l-5",
         "title": "Web Audio API実践的プログラミング",
         "speakers": ["keisuke_ai"],
         "description": "Web Audio APIの登場から2年ほど経ち、ゲームや楽器アプリなどさまざまな事例も増えてきました。またこの間に多くのブラウザが続々と対応し、Webでサウンドを直接扱うための実質的な標準仕様となりつつあります。$$$本セッションではWeb Audio APIの概要を紹介するとともに、本格的な音アプリの作成にあたり注意すべき点や、各種サウンドプログラミングのノウハウまで実例をまじえて紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=ln4-8FDIxI4",
+        "slides": null
       },
       {
         "id" : "5f-l-5",
         "title": "Webフロントエンドのレンダリングパフォーマンスと最適化Tips",
         "speakers": ["ayumu_sato"],
         "description": "Webフロントエンドのパフォーマンスは、今やページの初期表示を早くすることだけではありません。昨今のHTML/CSS/JavaScriptを駆使したWebコンテンツを、スムーズに動かすには、ブラウザのレンダリング(描画)処理について知る必要があります。このセッションでは、レンダリング上のよくあるボトルネックの見つけ方と対処を中心に、最適化Tipsをお届けします。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=cBT1Are3jXE",
+        "slides": null
       },
       {
         "id" : "5f-s0-5",
         "title": "エンタープライズ x HTML5 ～ 旧来型から次世代型へ、進化するWeb開発 ～",
         "speakers": ["yoshio_terada", "hiroshi_kawada"],
         "description": "jQueryの普及とHTML5の流行で、エンタープライズ領域でもフロントエンドのリッチ化が求められるようになりました。Web開発は、サーバサイドで画面遷移を実現する旧来型から、クライアントで画面遷移を実現する次世代型へと進もうとしています。本セッションでは、エンタープライズ向けHTML5製品から見たクライアントの開発スタイルの変化と、Java技術から見たサーバサイドの開発スタイルの変化をご紹介します。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=fzAOXTJOQZ0",
+        "slides": null
       },
       {
         "id" : "5f-s1-5",
         "title": "通信キャリア プロフェッショナルが語る HTML5 への期待",
         "speakers": ["kensaku_komatsu", "shin_miyakawa", "akihito_fujii", "ryuji_wakikawa"],
         "description": "HTML5を支えるインフラ環境が、$$$　- モバイルや車などのマルチデバイス化の進展$$$　- IPv4の枯渇とIPv6への移行によるNW環境の変化$$$　- IaaS, PaaS, BaaS といった多種多様なクラウドサービス$$$と大きく変容しています。これに伴い、Web開発にも様々な変化がおこることでしょう。$$$今回のパネルディスカッションでは、通信キャリア各社にて国内外で活躍する各分野のプロフェッショナルより、HTML5に対する要望や期待について伺います。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=lOQEYrTIaI4",
+        "slides": null
       },
       {
         "id" : "6f-l-5",
         "title": "Web をまともにしたいので Shadow DOM と Web Components をつくってます",
         "speakers": ["hayato_ito"],
         "description": "Web Componentsは、今年の Google I/O で注目を浴びた、Webアプリケーション開発の考え方を大きく変える期待の次世代Web技術です。「混ぜるな危険」状態である現在のWebアプリケーション開発の問題を解決するため、ブラウザ・ベンダはコンポーネント化のためWeb標準仕様の策定とブラウザへのネイティブ実装を進めています。これにより、Web開発者のみなさんはWeb技術でコンポーネントをつくり、それらに独自のHTMLのタグ名をつけて広く共有・再利用することが可能になります。$$$このセッションでは、Web Components の概要、そして現状について解説するとともに、Web Components を支える次世代Web技術について深く掘り下げていきます。$$$セッションのスピーカーは、Web Components の中核を支えるShadow DOM の Spec Editor であり、同時に Shadow DOM の Google Chrome への実装を行なっています。時間が許す限り、W3C Spec Editor の仕事や、Google Chrome の開発を普段どのように行なっているか、などについても触れたいと思います。",
-        "materials": null
+        "video": "http://www.youtube.com/watch?v=GCw4fJEEVe8",
+        "slides": null
       }
     ]
   },
