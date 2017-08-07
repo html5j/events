cc6ee4a96b3b56e37952993cf39bf8de90e45ab9
pikotea <pikotea@gmail.com>
2017-08-08 00:54:32 +0900

    モバイル版のメニューが動かないのを修正

--- public/conference/2017/9/assets/script/page-nav.js
+++ public/conference/2017/9/assets/script/page-nav.js
@@ -2,10 +2,10 @@
   'use strict';
 
   $( function () {
-    var $el = $( '.conf2016s-pageNav' );
+    var $el = $( '.conf2017s-pageNav' );
     var $list   = $el.find( 'ul' );
-    var $opener = $el.find( '.conf2016s-pageNav__opener' );
-    var modifier = 'conf2016s-pageNav--open';
+    var $opener = $el.find( '.conf2017s-pageNav__opener' );
+    var modifier = 'conf2017s-pageNav--open';
 
     $opener.on( 'click', function () {
       $.when(
