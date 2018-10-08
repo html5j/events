72326e42fbca1618ce3c8d559f50b4e2f3624bf5
namikuguri <zozo4869@gmail.com>
2018-10-09 02:30:02 +0900

    Use autoprefixer

--- conference/2018/11/gulpfile.js
+++ conference/2018/11/gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 const SRC_DIR = './src/style';
 const DEST_DIR = './assets/style';
 
-// SassとCssの保存先を指定
+// SassとCSSの保存先を指定
 gulp.task('sass', function(){
   gulp.src(`${SRC_DIR}/**/*.scss`)
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(gulp.dest(DEST_DIR));
 });
+
+// CSSにベンダープレフィックスを付与
+gulp.task('autoprefixer', () =>
+    gulp.src(`${DEST_DIR}/**/*.css`)
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(DEST_DIR))
+);
 
 //自動監視のタスクを作成(sass-watchと名付ける)
 gulp.task('sass-watch', ['sass'], function() {
