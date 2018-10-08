f92205780176e48ef3acbba5e201ecebe1f2f0b3
namikuguri <zozo4869@gmail.com>
2018-10-09 00:03:00 +0900

    Add responsive style

--- conference/2018/11/assets/script/page-nav.js
+++ conference/2018/11/assets/script/page-nav.js
@@ -1,46 +1,10 @@
-;( function ( $ ) {
+(function() {
   'use strict';
 
-  $( function () {
-    var $el = $( '.conf2018s-pageNav' );
-    var $list   = $el.find( 'ul' );
-    var $opener = $el.find( '.conf2018s-pageNav__opener' );
-    var modifier = 'conf2018s-pageNav--open';
+  var $handler = $('.js-draw-handler'),
+      $drawer = $('.js-drawer');
 
-    $opener.on( 'click', function () {
-      $.when(
-        $list.slideToggle( 200 )
-      ).done( function () {
-        if ( $list.is( ':visible') ) {
-          $el.addClass( modifier );
-        } else {
-          $el.removeClass( modifier );
-        }
-        $list.removeAttr( 'style' );
-      } );
-    } );
-  } );
-
-  function randomOrder(selector) {
-    var arr = [];
-    $(`${selector} li`).each(function() {
-      arr.push($(this));
-    });
-    arr.sort(function() {
-      return Math.random() - Math.random();
-    });
-    $(selector).empty();
-    arr.forEach(function(item) {
-      $(selector).append(item);
-    });
-  }
-  if ($('.conf2018s-inlineList').length !== 0) {
-    randomOrder('.conf2018s-inlineList.platinum');
-    randomOrder('.conf2018s-inlineList.gold');
-    randomOrder('.conf2018s-inlineList.silver');
-    randomOrder('.conf2018s-inlineList.bronze');
-    randomOrder('.conf2018s-inlineList.support');
-    randomOrder('.conf2018s-inlineList.media');
-  }
-
-} )( jQuery )
+  $handler.on('click', function() {
+    $drawer.toggleClass('is-open');
+  });
+})();
