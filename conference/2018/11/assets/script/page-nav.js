36c6a69a090af8e8a14479e6836ec800067ad6ea
pikotea <pikotea@gmail.com>
2018-10-19 13:41:40 +0900

    update supponsors

--- conference/2018/11/assets/script/page-nav.js
+++ conference/2018/11/assets/script/page-nav.js
@@ -7,4 +7,27 @@
   $handler.on('click', function() {
     $drawer.toggleClass('is-open');
   });
+
+  function randomOrder(selector) {
+    if ($(selector).length === 0) {
+      return;
+    }
+    var arr = [];
+    $(`${selector} li`).each(function() {
+      arr.push($(this));
+    });
+    arr.sort(function() {
+      return Math.random() - Math.random();
+    });
+    $(selector).empty();
+    arr.forEach(function(item) {
+      $(selector).append(item);
+    });
+  }
+  randomOrder('.thumb-list.platinum');
+  randomOrder('.thumb-list.gold');
+  randomOrder('.thumb-list.silver');
+  randomOrder('.thumb-list.bronze');
+  randomOrder('.thumb-list.support');
+  randomOrder('.thumb-list.media');
 })();
